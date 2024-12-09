4cffcd2913b6fb81da7224922e5cae655acd262f
Daniil <daniil.pchelov@mail.ru>
2024-12-09 22:45:20 +0300

    Добавление обработки данных

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from aiogram import Bot, Dispatcher, types, executor
 from aiogram.types.web_app_info import WebAppInfo
+import json
 
 bot = Bot('7820216850:AAHW3HqfkLH3hc4euNFnNx5-q48_XoD9qt8')
 dp = Dispatcher(bot)
@@ -10,6 +11,9 @@ async def start(message: types.Message):
     markup.add(types.KeyboardButton('Открыть веб страницу', web_app=WebAppInfo(url='https://omenx11.github.io/tg-apps/index.html')))
     await message.answer('Привет, мой друг!', reply_markup=markup)
 
-
+@dp.message_handler(content_types=['web_app_data'])
+async def web_app(message: types.Message):
+    res = json.loads(message.web_app_data.data)
+    await message.answer(f'Name: {res["name"]}. Email: {res["email"]}. Phone: {res["phone"]}')
 
 executor.start_polling(dp)
