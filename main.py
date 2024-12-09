458be7f8a8416632e39476e1e16c7b4f69d21edd
Daniil <daniil.pchelov@mail.ru>
2024-12-09 19:44:04 +0300

    Первый коммит

---
+++ main.py
@@ -0,0 +1,15 @@
+from aiogram import Bot, Dispatcher, types, executor
+from aiogram.types.web_app_info import WebAppInfo
+
+bot = Bot('7820216850:AAHW3HqfkLH3hc4euNFnNx5-q48_XoD9qt8')
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    markup.add(types.KeyboardButton('Открыть веб страницу', web_app=WebAppInfo(url='G:\Study\Курс\ТГ-бот\StudyBot\WebApps\index.html')))
+    await message.answer('Привет, мой друг!', reply_markup=markup)
+
+
+
+executor.start_polling(dp)
